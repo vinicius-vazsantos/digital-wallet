96e71f28ffbb1e472f3ed1f43a1302ae22aa31dd
Vinicius Vaz <viniciusvazdossantos747@gmail.com>
2025-08-23 10:50:37 -0300

    Ajuste de tipagem de propriedades da tabela.

--- backend/api/app/Model/AccountWithdrawPix.php
+++ backend/api/app/Model/AccountWithdrawPix.php
@@ -8,8 +8,8 @@ use Hyperf\DbConnection\Model\Model;
 use Hyperf\Database\Model\SoftDeletes;
 
 /**
- * @property int $id
- * @property int $account_withdraw_id
+ * @property string $id
+ * @property string $account_withdraw_id
  * @property string $type
  * @property string $key
  * @property \Carbon\Carbon $created_at
@@ -24,14 +24,15 @@ class AccountWithdrawPix extends Model
     public bool $incrementing = false;
 
     protected array $casts = [
-        'id' => 'integer',
-        'account_withdraw_id' => 'integer',
+        'id' => 'string',
+        'account_withdraw_id' => 'string',
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
         'deleted_at' => 'datetime',
     ];
 
     protected array $fillable = [
+        'id',
         'account_withdraw_id',
         'type',
         'key'
